3ca11ce8d27341da2d6f4600be2275f45023c6a2
rania <rania.yahmadi@sofrecom.com>
2021-11-29 16:35:27 +0100

    Working...

--- java/src/test/java/kata/PassWordTest.java
+++ java/src/test/java/kata/PassWordTest.java
@@ -3,7 +3,7 @@ package kata;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class PassWordTest {
+ class PassWordTest {
 	
 	@Test
 	 void pass_word_hasOneNumber_Valid(){
