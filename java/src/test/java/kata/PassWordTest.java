45f03873a251eb2b525273d743d3766e543d92f8
Ayoub Nasri <ayoub1.nasri@sofrecom.com>
2021-11-29 14:44:25 +0100

    Working...

--- java/src/test/java/kata/PassWordTest.java
+++ java/src/test/java/kata/PassWordTest.java
@@ -1,13 +1,15 @@
 package kata;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class PassWordTest {
 	
 	@Test
 	public void pass_word_valid(){
-		Password passWord = new Password();
-		
+		Password passWord = new Password("");
+
+		Assertions.assertThat(password.getPassWordValue())
 	}
 
 }
