ff859ff0617cd32894efdbbdab97d64b6c7854ec
Ayoub Nasri <ayoub1.nasri@sofrecom.com>
2021-11-29 14:29:23 +0100

    Working...

--- java/src/test/java/kata/KataShould.java
+++ java/src/test/java/kata/KataShould.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class PassWord {
+public class KataShould {
 
     @Test
     public void startWithAFailingTest() {
