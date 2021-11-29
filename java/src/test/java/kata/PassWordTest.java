bec445e3e7dd416e7a559e896c3db5571d0b1dca
Ayoub Nasri <ayoub1.nasri@sofrecom.com>
2021-11-29 15:52:01 +0100

    Working...

--- java/src/test/java/kata/PassWordTest.java
+++ java/src/test/java/kata/PassWordTest.java
@@ -1,10 +1,9 @@
+package kata;
 
 import org.junit.jupiter.api.Assertions;
 //import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-
-import kata.Password;
 
 public class PassWordTest {
 	
