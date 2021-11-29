492778e695610772ff536f7e2f6812b4953b9eff
rania <rania.yahmadi@sofrecom.com>
2021-11-29 15:40:12 +0100

    Working...

--- java/src/test/java/kata/KataShould.java
+++ java/src/test/java/kata/KataShould.java
@@ -3,8 +3,6 @@ package kata;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class KataShould {
 
     @Test
