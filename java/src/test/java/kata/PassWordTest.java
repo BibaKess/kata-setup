d0f95a0cd5f294a724f10e61a7a7908fca2d7c9b
Habiba Kessraoui <habiba.kessraoui@sofrecom.com>
2021-11-29 16:00:33 +0100

    Working...

--- java/src/test/java/kata/PassWordTest.java
+++ java/src/test/java/kata/PassWordTest.java
@@ -52,7 +52,7 @@ public class PassWordTest {
         Assertions.assertTrue("az".matches("[a-z]+"));
         Assertions.assertTrue("Aa".matches("[a-zA-Z]+"));
         Assertions.assertTrue("aZ".matches("[a-zA-Z]+"));
-        Assertions.assertTrue("1Z".matches("[a-zA-Z]+"));
+        Assertions.assertTrue("1Z".matches("1[a-zA-Z]+"));
 //        Assertions.assertTrue("a".matches("/^([a-zA-Z]+)$/"));
 //        Assertions.assertTrue("az".matches("/^([a-zA-Z]+)$/"));
     }
