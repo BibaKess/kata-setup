a85986b583752230413ee6ed11a9335d8c9ae102
johan.martinsson <johan.martinsson@dexyos.fr>
2021-11-29 15:23:13 +0100

    Working...

--- java/src/test/java/kata/PassWordTest.java
+++ java/src/test/java/kata/PassWordTest.java
@@ -44,7 +44,7 @@ public class PassWordTest {
 
     @Test
     void name() throws Exception {
-        Assertions.assertTrue("a".matches("/(a+)/"));
+        Assertions.assertTrue("a".matches("/(a)/"));
         Assertions.assertTrue("a".matches("/^([a-zA-Z]+)$/"));
         Assertions.assertTrue("az".matches("/^([a-zA-Z]+)$/"));
     }
